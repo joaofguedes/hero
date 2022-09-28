d039e4425e138d8d19ea8064dc5c486f968b0d03
Joao Guedes <joao.f.s.guedes@gmail.com>
2022-09-28 16:17:27 +0100

    New version

---
+++ src/main/java/Application.java
@@ -0,0 +1,23 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            Terminal terminal = new
+                    DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
+        } catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+}

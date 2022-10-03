a3e13de386fa8924706e152895d13df23420c0df
Joao Guedes <up202108711@up.pt>
2022-10-03 16:01:15 +0100

    Added processKey

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -37,6 +38,12 @@ public class Game {
 
     public void run() throws IOException{
             draw();
-        }
+            KeyStroke key = screen.readInput();
+            processKey(key);
     }
+
+    private void processKey(KeyStroke key){
+        System.out.println(key);
+    }
+}
 
